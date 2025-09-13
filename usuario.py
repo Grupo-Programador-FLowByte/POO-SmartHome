e277c092bc31a75d08a5b27deaf66e8d433fed0c
Lucas <lggp_1312@hotmail.com>
2025-09-13 15:56:00 -0300

    subiendo division de tareas: clases y tests

---
+++ usuario.py
@@ -0,0 +1,23 @@
+"""
+Tareas de Franco
+
+Clase Usuario
+
+1. Crear la clase Usuario con los siguientes atributos:
+   - id_usuario
+   - nombre
+   - usuario
+   - clave
+   - rol
+
+2. Implementar métodos:
+   - registrar_usuario()
+   - iniciar_sesion()
+   - cambiar_rol_usuario()
+   - obtener_usuario_por_nombre()
+   - getters y setters
+
+3. Crear los tests en tests/test_usuario.py siguiendo TDD:
+   - Primero escribir tests que fallen.
+   - Luego implementar métodos para pasar los tests.
+"""
